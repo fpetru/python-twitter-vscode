f0c4128943a3b84c6d76ba47b09a7e2534494d0b
petru_000 <petru_000@MAGDAWIN>
2016-12-20 09:52:15 +0100

    updates: blacklist, tweets uniqueness

--- retrieve.py
+++ retrieve.py
@@ -2,12 +2,17 @@
 """This script connects and extract from Twitter more messages (tweets) as json file
 """
 import json
-import configparser 
+import time
+import configparser
 from TwitterAPI import TwitterAPI
 from TwitterAPI import TwitterRestPager
+from datetime import timedelta
+
+start = time.time()
+print("starting to retrieve tweets...")
 
 configValues = configparser.RawConfigParser()
-configValues.read(r'.\config.ini')
+configValues.read(r'.\config.real.ini')
 
 TWITTER_API = TwitterAPI(configValues.get('TwitterSettings', 'consumer_key'),
                          configValues.get('TwitterSettings', 'consumer_secret'),
@@ -19,19 +24,34 @@ TWITTER_PAGER = TwitterRestPager(TWITTER_API, \
                                 {'q':'#webapi', 'count':10, 'lang': 'en'})
 
 response = []
+unique_tweets = []
+itemId = 1
+
+# black list items: exclude all items referring to job ads
+rejected_words = ['hir', 'need help', 'developer', 'contact', 'job']
+
 for item in TWITTER_PAGER.get_iterator():
     if 'user' in item and 'text' in item:
-        response.append({'username': item['user']['name'],
-                         'screen_name': '@{}'.format(item['user']['screen_name']),
-                         'profile_image': item['user']['profile_image_url_https'],
-                         'profile_description': item['user']['description'],
-                         'text': item['text']})
+        found_words = [c for c in rejected_words if c in item['text'].lower()]
+        if len(found_words) == 0 and item['text'] not in unique_tweets:
+            response.append({'Id': itemId,
+                             'Username': item['user']['name'],
+                             'ScreenName': '@{}'.format(item['user']['screen_name']),
+                             'ProfileImage': item['user']['profile_image_url_https'],
+                             'ProfileDescription': item['user']['description'],
+                             'Text': item['text']})
+            unique_tweets.append(item['text'])
+            itemId += 1
+
     elif 'message' in item and item['code'] == 88:
         print('SUSPEND, RATE LIMIT EXCEEDED: %s\n' % item['message'])
         break
 
-    if len(response) > 5:
+    if len(response) > 99:
         break
 
-with open(r'.\twitter.txt', 'w') as outfile:
+with open(r'.\twitter.json', 'w') as outfile:
     json.dump(response, outfile, indent=4)
+
+end = time.time()
+print('found %d tweets in %s' % (len(response), str(timedelta(seconds=end-start)))
